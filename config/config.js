a5c1e04df58352f434fc4fbb121594015ffdf9e4
Egon Holtz <egonholtz@gmail.com>
2022-02-06 13:04:49 +0000

    Creating server with express + mongoose

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    mongodb: {
+        uri: 'mongodb+srv://AutoMngmentAdm:KUS7eC7TR3t52NqV@automanagementclstr.xzsgr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        collections: {
+            cars: 'cars',
+            user: 'users',
+        }        
+    },
+    auth: {
+        expiration_time: 9999999,
+        issuer: "EHO"
+    },
+    sanitize: {
+        alphabet: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËẼÌÍÎÏĨÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëẽìíîïĩðñòóôõöøùúûüýÿ\\ ",
+        numerical: "0123456789"
+    },
+    email: {
+        service: "Gmail",
+        auth: {
+            user: "mailserverpw@gmail.com",
+            pass: "ttxirdxzkafhcuel"
+        }
+    }
+}
